9159b568a6e3381e67866bb248ccb6ec644167e0
Konstantin Dankov <konstantin.dankov@gmail.com>
2010-03-08 23:49:30 +0200

    Added logo to the header
    Updates to the localization
    
    Added speacial case to create the Table of Contents

--- page.php
+++ page.php
@@ -13,6 +13,7 @@
 
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<div class="entry-head">
+
 					<h1 class="entry-title">
 						<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php k2_permalink_title(); ?>"><?php the_title(); ?></a>
 					</h1>
@@ -20,6 +21,7 @@
 					<?php /* Edit Link */ edit_post_link(__('Edit','k2_domain'), '<span class="entry-edit">', '</span>'); ?>
 
 					<?php /* K2 Hook */ do_action('template_entry_head'); ?>
+
 				</div><!-- .entry-head -->
 
 				<div class="entry-content">
@@ -35,21 +37,27 @@
 			
 			<?php
 				global $wpdb;
-				$querystr = "SELECT ID, post_title, guid FROM $wpdb->posts WHERE post_type = 'page' AND post_status = 'publish' AND post_parent = ".$post->ID." ORDER BY post_title";
+				if( $post->ID == 5 ){
+					$querystr = "SELECT ID, post_title, guid FROM $wpdb->posts WHERE post_type = 'page' AND post_status = 'publish' AND post_parent = ".$post->ID." ORDER BY menu_order";
+				}else{
+					$querystr = "SELECT ID, post_title, guid FROM $wpdb->posts WHERE post_type = 'page' AND post_status = 'publish' AND post_parent = ".$post->ID." ORDER BY post_title";
+				}
 				$pageposts = $wpdb->get_results($querystr, OBJECT);
 			?>
 			
 			<?php if( count($pageposts) > 0 ){ ?>
-			<div class="matrix">	
+			<div class="<?php
+			if($post->ID == 5){ ?>
+				toc
+			<?php }else{ ?>
+				matrix
+			<?php } ?>">	
 				<ol>
 					<?php
 					foreach( $pageposts as $page )
 					{ ?>
 						<li>
-							<a href="<?php echo get_permalink( $page->ID ) ?>">
-								<?php echo get_the_post_thumbnail( $page->ID ); ?>
-								<span class="title"><?php echo $page->post_title; ?></span>
-							</a>
+							<a href="<?php echo get_permalink( $page->ID ) ?>"><?php echo get_the_post_thumbnail( $page->ID ); ?><span class="title"><?php echo $page->post_title; ?></span></a>
 						</li>
 					<?php
 					} ?>
