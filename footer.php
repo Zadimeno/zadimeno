2bad8968dca8b2a183ceb2974fb450ce9ceff633
Konstantin Dankov <konstantin.dankov@gmail.com>
2010-03-20 11:13:41 +0200

    Fixed .matrix hiding of the left border
    Added the license text in the footer

--- footer.php
+++ footer.php
@@ -10,8 +10,10 @@
 <div id="footer">
 
 	<?php locate_template( array('blocks/k2-footer.php'), true ); ?>
-
+	
 	<?php /* K2 Hook */ do_action('template_footer'); ?>
+	<br />
+	<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/2.5/bg/">Текстовете са под лиценз BY-NC-ND</a>.
 </div><!-- #footer -->
 
 <?php wp_footer(); ?>
